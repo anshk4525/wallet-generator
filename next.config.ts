6a408a1d1abe6adfd8fe6f631089ffb50acbf82f
Ansh kumar <derivative@Mac.lan>
2025-02-01 09:08:23 -0500

    Updated <Walletgenerator> with changes

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Disable Turbopack
   experimental: {
-    appDir: true, // Enable if you're using the App Router
-    turbopack: false, // Explicitly disable Turbopack
+    // appDir: true, // Enable if you're using the App Router
+    // turbopack: false, // Explicitly disable Turbopack
   },
   // Custom Webpack configuration (optional)
   webpack: (config) => {
